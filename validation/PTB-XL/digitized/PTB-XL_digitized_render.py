d760ffe069e0aa36a3fd6ec9f1d4a4a0a20b796a
adofersan <adolfwiner@gmail.com>
2025-07-14 00:21:08 +0200

    NOP

--- validation/PTB-XL/digitized/PTB-XL_digitized_render.py
+++ validation/PTB-XL/digitized/PTB-XL_digitized_render.py
@@ -2,15 +2,14 @@ from os import listdir
 from os.path import abspath, join
 import sys
 
-sys.path.insert(1, abspath(__file__ + 4 * "/..") + "\src")
+sys.path.insert(1, abspath(__file__ + 4 * "/.."))
 import pandas as pd
 from tqdm import tqdm
-from validation.render import ECGRenderer
+from validation.render import render
 
 if __name__ == "__main__":
-    INPUT_DIR = r"./validation/PTB-XL/digitized/signal"
+    INPUT_DIR = r"./validation/PTB-XL/original/signal"
     OUTPUT_DIR = r"./validation/PTB-XL/digitized/img"
-    ecg_r = ECGRenderer(sample_rate=500, ref_pulse_at_right=True)
     dir_list = listdir(INPUT_DIR)
     for i in tqdm(range(len(dir_list)), desc="Progress"):
         file = dir_list[i]
@@ -32,4 +31,9 @@ if __name__ == "__main__":
             "V6",
         ]
         output_fname = file_id + ".png"
-        ecg_r.render(signals, OUTPUT_DIR + "/" + output_fname)
+        render(
+            signals,
+            OUTPUT_DIR + "/" + output_fname,
+            sample_rate=500,
+            ref_pulse_at_right=True,
+        )
